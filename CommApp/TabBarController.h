bd0869b4889c92903cfc4d0c579032c712a80631
Game <hl0614020104@126.com>
2015-07-22 00:24:30 +0800

    ccc
    ccc

--- CommApp/TabBarController.h
+++ CommApp/TabBarController.h
@@ -10,9 +10,21 @@
 
 @interface TabBarController : UITabBarController
 
-@property(nonatomic, strong) IBOutlet UIButton *centerButton;
-- (void)buttonPressed:(id)sender;
+@property (nonatomic, strong) IBOutlet UIButton  *m_centerBtn; //Add button
+//The View that contains gouyigou and piaoliuping is displayed or not.
+@property (nonatomic)        bool                 m_isPopupViewDisplay;
+@property (nonatomic,strong) UIView              *m_popupView;
+
+
+-(void)initialize;//Properties initialize.
+-(void)buttonPressed:(id)sender;
 -(void)changeTabBarView;
--(void)pressedPopupWindow;   //Press add logo,the edit tab would be poped up;
+-(void)closePopupWindow;
+-(void)addBtnOnPopupWindowWithTag:(NSInteger)tag NormalImage:(UIImage*)btnImage HighlightedImage:(UIImage*)hlImage PopupView:(UIView * )popupView andCenter:(CGPoint )center;
+-(void)buttonOnPopupView:(UIButton *)btn;
+
+//-------------------------------------
+//Input function
+-(void)displayInputFiled;
 
 @end
--- CommApp/TabBarController.m
+++ CommApp/TabBarController.m
@@ -9,18 +9,29 @@
 #import "TabBarController.h"
 
 @interface TabBarController ()
-@property (retain, nonatomic) IBOutlet UITabBar *tabBarView;
+@property (retain, nonatomic) IBOutlet UITabBar *m_tabBarView;
 
 @end
 
 @implementation TabBarController
-@synthesize tabBarView=_tabBarView;
-@synthesize centerButton;
+
+@synthesize m_tabBarView=_m_tabBarView;
+@synthesize m_popupView=_m_popupView;
+
+@synthesize m_centerBtn;  //Plus Button
+
+@synthesize m_isPopupViewDisplay;
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    [self initialize];
     [self changeTabBarView];
     [self addCenterButtonWithImage:[UIImage imageNamed:@"hood.png"] highlightImage:[UIImage imageNamed:@"hood_selected.png"] target:self action:@selector(buttonPressed:)];
+}
+
+-(void)initialize
+{
+    self.m_isPopupViewDisplay=false;
 }
 
 - (void)didReceiveMemoryWarning {
@@ -31,6 +42,23 @@
 -(void)changeTabBarView
 {
     self.tabBar.tintColor=[UIColor blackColor];
+}
+
+-(void)addBtnOnPopupWindowWithTag:(NSInteger)tag NormalImage:(UIImage *)btnImage HighlightedImage:(UIImage *)hlImage PopupView:(UIView *)popupView andCenter:(CGPoint)center
+{
+    UIButton* popupBtn=[UIButton buttonWithType:UIButtonTypeCustom];
+    popupBtn.autoresizesSubviews=UIViewAutoresizingFlexibleRightMargin | UIViewAutoresizingFlexibleLeftMargin | UIViewAutoresizingFlexibleBottomMargin | UIViewAutoresizingFlexibleTopMargin;
+    
+    CGFloat popupBtnRatio=btnImage.size.width/btnImage.size.height;
+    popupBtn.bounds=CGRectMake(0.0f,0.0f,popupView.frame.size.height*popupBtnRatio,popupView.frame.size.height);
+    [popupBtn setBackgroundImage:btnImage forState:UIControlStateNormal];
+    [popupBtn setBackgroundImage:hlImage forState:UIControlStateHighlighted];
+    [popupBtn addTarget:self action:@selector(buttonOnPopupView:) forControlEvents:UIControlEventTouchUpInside];
+    popupBtn.center=CGPointMake(popupView.center.x+center.x, popupView.center.y+center.y);
+    popupBtn.layer.zPosition=2;
+    [popupBtn setTag:tag];
+    
+    [self.view addSubview:popupBtn];
 }
 
 - (void)addCenterButtonWithImage:(UIImage *)buttonImage highlightImage:(UIImage *)highlightImage target:(id)target action:(SEL)action
@@ -56,29 +84,71 @@
     button.layer.zPosition=1; //Make add button on the above layer
     
     [self.view addSubview:button];
-    self.centerButton = button;
 }
+
 
 - (void)buttonPressed:(id)sender
 {
-    NSLog(@"pressed");
-    [self pressedPopupWindow];
+    if (self.m_isPopupViewDisplay==false)
+    {
+        [self showPopupWindow];
+        self.m_isPopupViewDisplay=true;
+    }
+    else
+    {
+        [self closePopupWindow];
+        self.m_isPopupViewDisplay=false;
+    }
+    
+    
 }
 
-
--(void)pressedPopupWindow
+-(void)buttonOnPopupView:(UIButton *)btn
 {
-    UIView *popupView=[[UIView alloc]initWithFrame:CGRectMake(0, self.tabBar.center.y-self.tabBar.bounds.size.height-25.0f,[[UIScreen mainScreen]bounds].size.width, 50.0f)];
-    popupView.backgroundColor=[UIColor colorWithRed:42.0/255 green:0.0/255 blue:48.0/255 alpha:0.9];
-    [self.view addSubview:popupView];
+    switch (btn.tag) {
+        case 111:NSLog(@"Gouyigou"); [self displayInputFiled];break;     //when tag is 111,the click button is Gou yi Gou.
+        case 222:NSLog(@"piaoliuping");break;   //when tag is 222,the click button is piao liu ping.
+        default:break;
+    }
 }
 
 
+-(void)showPopupWindow
+{
+    self.m_popupView=[[UIView alloc]initWithFrame:CGRectMake(0, self.tabBar.center.y-self.tabBar.bounds.size.height-10.0f,[[UIScreen mainScreen]bounds].size.width, 35.0f)];
+    self.m_popupView.backgroundColor=[UIColor colorWithRed:42.0/255 green:0.0/255 blue:48.0/255 alpha:1];
+    [self.view addSubview:self.m_popupView];
+ 
+    [self addBtnOnPopupWindowWithTag:111 NormalImage:[UIImage imageNamed:@"gouyigou.png"] HighlightedImage:[UIImage imageNamed:@"gouyigou_selected.png"] PopupView:self.m_popupView andCenter:CGPointMake(-80.0f, 0.0f)];
+    
+    [self addBtnOnPopupWindowWithTag:222 NormalImage:[UIImage imageNamed:@"piaoliuping.png"] HighlightedImage:[UIImage imageNamed:@"piaoliuping_selected.png"] PopupView:self.m_popupView andCenter:CGPointMake(80.0f,0.0f)];
+    
+    
+    
+}
+
+-(void)closePopupWindow
+{
+    [self.m_popupView removeFromSuperview];
+    [self.m_popupView release];
+    UIButton *gouyigouBtn=(UIButton *)[self.view viewWithTag:111];
+    UIButton *piaoliupingBtn=(UIButton *)[self.view viewWithTag:222];
+    [gouyigouBtn removeFromSuperview];
+    [piaoliupingBtn removeFromSuperview];
+}
 
 
+-(void)displayInputFiled
+{
+    UIView *inputView=[[UIView alloc]init];
+    inputView.frame=CGRectMake(0, 0, [[UIScreen mainScreen]bounds].size.width,[[UIScreen mainScreen]bounds].size.height/6 );
+    [inputView setBackgroundColor:[UIColor blackColor]];
+    inputView.center=CGPointMake(self.tabBar.center.x, self.tabBar.center.y-self.m_popupView.frame.size.height-[[UIScreen mainScreen]bounds].size.height/8);
+    [self.view addSubview:inputView];
+}
 
 - (void)dealloc {
-    [_tabBarView release];
+    [_m_tabBarView release];
     [super dealloc];
 }
 
