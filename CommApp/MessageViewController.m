7098e4ae36090834c83cd0cc3147764b2634dd29
Game <hl0614020104@126.com>
2015-07-28 00:34:55 +0800

    add chat list
    add chat list

--- CommApp/MessageViewController.m
+++ CommApp/MessageViewController.m
@@ -10,17 +10,29 @@
 #import "EaseMob.h"
 #import "NavigationBarMgr.h"
 
-@interface MessageViewController ()<UIAlertViewDelegate,EMChatManagerLoginDelegate,IChatManagerDelegate> //登录对话框响应
-@property (retain, nonatomic) IBOutlet UITextField *HuanXinUsername;//登陆用户，用户名
-@property (retain, nonatomic) IBOutlet UITextField *HuanXinpassword;//登陆用户 密码
+@interface MessageViewController ()<UIAlertViewDelegate,EMChatManagerLoginDelegate,IChatManagerDelegate,UITableViewDelegate,UITableViewDataSource> //登录对话框响应
 
+@property (retain,nonatomic) UITextField *usernameHuanXinTextField;
+
+@property (retain,nonatomic) UITextField *passwordHuanXinTextField;
+@property (retain, nonatomic) IBOutlet UITextField *MessageTextField;
+@property (retain, nonatomic) IBOutlet UITableView *HuanXinChatView; //对话列表
+
+@property (retain,nonatomic) NSMutableArray *chatList;
+@property (assign) bool flag;
 
 @end
 
 @implementation MessageViewController
+@synthesize usernameHuanXinTextField=_usernameHuanXinTextField;
+@synthesize passwordHuanXinTextField=_passwordHuanXinTextField;
+@synthesize chatList=_chatList;
+
+
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    [self Initialize];
     [self addNavitionBar];
     [[EaseMob sharedInstance].chatManager addDelegate:self delegateQueue:nil];
     [self popUpLoginAndRegisterAlertView];
@@ -29,6 +41,12 @@
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
+}
+
+-(void)Initialize //数据初始化
+{
+    self.chatList=[[NSMutableArray alloc]init];
+    self.flag=YES;
 }
 
 -(void)popUpLoginAndRegisterAlertView  //输入用户名密码登录
@@ -43,8 +61,9 @@
                            
                                                       otherButtonTitles:@"登录",@"注册",nil];
     [alert setAlertViewStyle:UIAlertViewStyleLoginAndPasswordInput];
-    UITextField *usernameTextField=[alert textFieldAtIndex:0];
-    UITextField *passwordTextField=[alert textFieldAtIndex:1];
+    self.usernameHuanXinTextField=[alert textFieldAtIndex:0];
+    self.passwordHuanXinTextField=[alert textFieldAtIndex:1];
+    [alert setTag:1000];//设置登录框标签为1000
     [alert show];
     
 
@@ -52,7 +71,52 @@
 
 -(void)alertView:(UIAlertView *)alertView clickedButtonAtIndex:(NSInteger)buttonIndex  //响应登录框提交事件
 {
-    NSLog(@"%ld",(long)buttonIndex);
+    if (alertView.tag==1000)
+    {
+           if (buttonIndex==1)
+           {
+             //登录
+             [[EaseMob sharedInstance].chatManager asyncLoginWithUsername:self.usernameHuanXinTextField.text
+                                                                 password:self.passwordHuanXinTextField.text];
+           }
+          else if(buttonIndex==2)
+          {
+             //注册
+             [[EaseMob sharedInstance].chatManager asyncRegisterNewAccount:self.usernameHuanXinTextField.text
+                                                                 password:self.passwordHuanXinTextField.text];
+          }
+          else
+          {
+              NSLog(@"错误");
+          }
+     }
+    else if(alertView.tag==2000)
+    {   //重新登录
+        if (buttonIndex==1)
+        {
+            [self popUpLoginAndRegisterAlertView];
+        }
+    }
+    else if(alertView.tag==3000)
+    {   //重新注册
+        if (buttonIndex==1)
+        {
+            [self popUpLoginAndRegisterAlertView];
+        }
+    }
+    else if(alertView.tag==4000)
+    {   //注册成功，登录
+        if (buttonIndex==1)
+        {
+            [self popUpLoginAndRegisterAlertView];
+        }
+    }
+    else
+    {
+        NSLog(@"ERROR");
+    }
+    
+    
 }
 
 
@@ -61,17 +125,6 @@
     NavigationBarMgr* navBar=[NavigationBarMgr sharedInstance];
     UINavigationBar *bar=[navBar getNavigationBar];
     [self.view addSubview:bar];
-}
-
-- (IBAction)HuanXinRegister:(id)sender
-{
-    [[EaseMob sharedInstance].chatManager asyncRegisterNewAccount:self.HuanXinUsername.text password:self.HuanXinpassword.text];
-}
-
-- (IBAction)HuanXinLogin:(id)sender
-{
-    [[EaseMob sharedInstance].chatManager asyncLoginWithUsername:self.HuanXinUsername.text
-                      password:self.HuanXinpassword.text];
 }
 
 
@@ -94,13 +147,16 @@
                 default:                                       errorChnDescription=@"未知错误";break;
         }
         
-        UIAlertView *AlertView=[[UIAlertView alloc]initWithTitle:@"登陆失败" message:errorChnDescription delegate:nil
-                                                       cancelButtonTitle:@"好" otherButtonTitles:nil];
+        UIAlertView *AlertView=[[UIAlertView alloc]initWithTitle:@"登陆失败" message:errorChnDescription delegate:self
+                                                    cancelButtonTitle:@"取消" otherButtonTitles:@"重新登录",nil];
+        [AlertView setTag:2000];
         [AlertView show];
     }
     else
     {
-      
+        UIAlertView *AlertView=[[UIAlertView alloc]initWithTitle:@"登陆成功" message:@"登录成功" delegate:self
+                                               cancelButtonTitle:@"好" otherButtonTitles:nil];
+        [AlertView show];
     }
    
     
@@ -130,16 +186,18 @@
                 default:                             errorChnDescription=@"未知错误";break;
         }
         
-        UIAlertView *AlertView=[[UIAlertView alloc]initWithTitle:@"注册失败" message:errorChnDescription delegate:nil
-                                                       cancelButtonTitle:@"好" otherButtonTitles:nil];
+        UIAlertView *AlertView=[[UIAlertView alloc]initWithTitle:@"注册失败" message:errorChnDescription delegate:self
+                                                       cancelButtonTitle:@"好" otherButtonTitles:@"重新注册",nil];
+        [AlertView setTag:3000];
         [AlertView show];
         NSLog(@"注册失败");
 
     }
     else
     {
-        UIAlertView *scussessAlertView=[[UIAlertView alloc]initWithTitle:@"注册成功" message:@"恭喜您注册成为勾勾会员" delegate:nil
-                                                           cancelButtonTitle:@"好" otherButtonTitles:nil];
+        UIAlertView *scussessAlertView=[[UIAlertView alloc]initWithTitle:@"注册成功" message:@"恭喜您注册成为勾勾会员" delegate:self
+                                                       cancelButtonTitle:@"好" otherButtonTitles:@"登录",nil];
+        [scussessAlertView setTag:4000];
         [scussessAlertView show];
         NSLog(@"注册成功");
     }
@@ -156,7 +214,9 @@
             switch (messagebody.messageBodyType) {
                 case eMessageBodyType_Text:
                     [self dealwithTextMessage:(EMTextMessageBody *)messagebody];
+                    [self.chatList addObject:((EMTextMessageBody *)messagebody).text];
                 NSLog(@"output+%@",((EMTextMessageBody *)messagebody).text);
+                    [self.HuanXinChatView reloadData];
                 break;
                 default:break;
             }
@@ -173,19 +233,42 @@
 
 - (IBAction)HuanXINSend:(id)sender
 {
-    EMChatText *texChat=[[EMChatText alloc] initWithText:@"abcdefg"];
+    [self.chatList addObject:self.MessageTextField.text];
+    EMChatText *texChat=[[EMChatText alloc] initWithText:self.MessageTextField.text];
     EMTextMessageBody *body=[[EMTextMessageBody alloc]initWithChatObject:texChat];
-    EMMessage *message=[[EMMessage alloc]initWithReceiver:self.HuanXinUsername.text bodies:@[body]];
+    EMMessage *message=[[EMMessage alloc]initWithReceiver:self.usernameHuanXinTextField.text bodies:@[body]];
     [message setMessageType:eMessageTypeChat]; //设置为单聊模式
     [message setDeliveryState:eMessageDeliveryState_Delivered];
     [[EaseMob sharedInstance].chatManager sendMessage:message progress:nil error:nil];
 }
 
+//----------------------------------------------
+//对话列表
+-(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *CellWithIdentifier = @"Cell";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellWithIdentifier];
+    NSUInteger row = [indexPath row];
+    if (cell==nil)
+    {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleValue2 reuseIdentifier:CellWithIdentifier];
+    }
+    cell.textLabel.text=[self.chatList objectAtIndex:row];
+    return cell;
+    
+}
+
+-(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return  [self.chatList count];
+}
 
 
-- (void)dealloc {
-    [_HuanXinUsername release];
-    [_HuanXinpassword release];
+
+- (void)dealloc
+{
+    [_MessageTextField release];
+    [_HuanXinChatView release];
     [super dealloc];
 }
 @end
