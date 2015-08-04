121c9991f0060890b34c76ef1363786893aedf5c
Lance0226 <lance@lances-Mac-mini.local>
2015-08-04 15:17:44 +0800

    add image
    add image

--- CommApp/PiaoLiuPing.m
+++ CommApp/PiaoLiuPing.m
@@ -30,6 +30,40 @@
     
     [self presentViewController:picker animated:YES completion:NULL];
 }
+- (IBAction)saveImage:(id)sender
+{
+    [self saveImg:self.imageView.image];
+    
+}
+
+-(void)saveImg:(UIImage*)image
+{
+    if (image!=nil) {
+        NSArray *paths=NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
+        NSString *documentDirecotry=[paths objectAtIndex:0];
+        NSString *path=[documentDirecotry stringByAppendingPathComponent:@"test.png"];
+        NSLog(@"%@",path);
+        NSData* data=UIImagePNGRepresentation(image);
+        [data writeToFile:path atomically:YES];
+        
+    }
+}
+- (IBAction)loadImg:(id)sender
+{
+    UIImage *img=[self loadImage];
+    self.imageView.image=img;
+}
+
+- (UIImage*)loadImage
+{
+    NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory,
+                                                         NSUserDomainMask, YES);
+    NSString *documentsDirectory = [paths objectAtIndex:0];
+    NSString *path = [documentsDirectory stringByAppendingPathComponent:
+                      @"test.png" ];
+    UIImage *image = [UIImage imageWithContentsOfFile:path];
+    return image;
+}
 
 - (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary *)info {
     
